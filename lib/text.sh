d86f0a2cf59a35f7a8ce86a87b5a1170b53e9a29
Tyler Auer <Tyler.Auer@gmail.com>
2023-12-21 07:05:20 -0700

    Add comments

--- lib/text.sh
+++ lib/text.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# Converts string to lowercase
 function lowercase() {
     echo $1 | tr '[:upper:]' '[:lower:]'
 }
 
+# Applies a Caesar cipher to a string
+# Usage: caeserCipher <shift> <string>
 function caeserCipher() {
     local shift=$1
     local string=$2
